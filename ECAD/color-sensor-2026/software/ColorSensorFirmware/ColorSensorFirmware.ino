7afd4476620dd5fa6623c43e5bce495ccfa0b4ec
Barbatronic <adrien.bracq@gmail.com>
2026-01-18 00:11:36 +0100

    firmware - serial version

---
+++ ECAD/color-sensor-2026/software/ColorSensorFirmware/ColorSensorFirmware.ino
@@ -0,0 +1,76 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIXEL_PIN   PIN_PA3
+#define SENSOR_PIN  PIN_PA1
+#define TIME_GET_COLOR 30
+
+Adafruit_NeoPixel pixel(1, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+static uint16_t avgRead(uint8_t n) {
+  uint32_t s = 0;
+  for (uint8_t i = 0; i < n; i++) s += analogRead(SENSOR_PIN);
+  return (uint16_t)(s / n);
+}
+
+void setup() {
+  pixel.begin();
+  pixel.setBrightness(20);
+  pixel.setPixelColor(0, 0);  // off
+  pixel.show();
+
+  pinMode(SENSOR_PIN, INPUT);
+  analogReference(DEFAULT);
+  analogRead(SENSOR_PIN);     // dummy
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  uint16_t ambient, r, g, b;
+  char c = 'u';
+
+  // OFF (ambient)
+  pixel.setPixelColor(0, 0);
+  pixel.show();
+  delay(TIME_GET_COLOR);
+  ambient = avgRead(16);
+
+  // R
+  pixel.setPixelColor(0, 255, 0, 0);
+  pixel.show();
+  delay(TIME_GET_COLOR);
+  r = avgRead(8);
+  if (r > ambient) r -= ambient; else r = 0;
+
+  // G
+  pixel.setPixelColor(0, 0, 255, 0);
+  pixel.show();
+  delay(TIME_GET_COLOR);
+  g = avgRead(8);
+  if (g > ambient) g -= ambient; else g = 0;
+
+  // B
+  pixel.setPixelColor(0, 0, 0, 255);
+  pixel.show();
+  delay(TIME_GET_COLOR);
+  b = avgRead(8);
+  if (b > ambient) b -= ambient; else b = 0;
+
+  // LED OFF
+  pixel.setPixelColor(0, 0);
+  pixel.show();
+
+  // Decision (dominante)
+  if (r > g && r > b) c = 'r';
+  else if (g > r && g > b) c = 'g';
+  else if (b > r && b > g) c = 'b';
+
+  // Sortie ultra compacte: A R G B C
+  Serial.print(ambient); Serial.print(' ');
+  Serial.print(r);       Serial.print(' ');
+  Serial.print(g);       Serial.print(' ');
+  Serial.print(b);       Serial.print(' ');
+  Serial.println(c);
+
+  delay(200);
+}
